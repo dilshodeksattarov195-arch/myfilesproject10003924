0e03dd634e0f339d80f0ee7447e51aba82f76c87
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-28 11:54:03 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class cacheDyncWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = cacheDyncWorker()
+    print("Worker engine initialized.")
